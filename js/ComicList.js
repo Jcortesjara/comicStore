2ba417580699fa7699c71baae149c3b4c4c012f3
Jose  Cortes Jaramillo <josecortesjaramillo@gmail.com>
2016-08-14 10:27:13 -0500

    no pain no gain

--- js/ComicList.js
+++ js/ComicList.js
@@ -1,5 +1,6 @@
 var ComicList = (function () {
   var comicArray = [];
+  var comicListGlobal;
 
 
   // funcion que inicia la peticion
@@ -8,13 +9,19 @@ var ComicList = (function () {
   };
 
   var searchBar = function () {
-    var comic = $('#buscador').val();
-    comic.toUpperCase();
+    var comic = $('#buscador').val().toUpperCase();
     if (comic.length > 3) {
       for (var index = 0; index < comicArray.length; index++) {
         var comicExpression = new RegExp(comicArray[index])
-        var result = comic.match(comicExpression)
+        var result = comic.match(comicExpression);
+        if (result != undefined) {
+          var title = '<button class="comicButton" id=' + result[0].toLowerCase() + '>' + result[0].substr(0, 1).toUpperCase() + result[0].substr(1).toLowerCase() + '</button>';
+          $('.panel-list').html(title);
+          showDetails(result[0].toLowerCase(), comicListGlobal);
+        }
       }
+    } else {
+      init();
     }
   }
 
@@ -40,31 +47,37 @@ var ComicList = (function () {
 
   //Obtiene la lista de comics que le peticion _requestComics realizo
   var getComics = function (data) {
+    $('.panel-list').html("");
     var comicList = data.comicList;
+    comicListGlobal = comicList;
     for (var index = 0; index < comicList.length; index++) {
       showComicList(comicList[index], comicList);
       comicArray.push(comicList[index].title.toUpperCase());
+
     }
   };
 
 
   var showComicList = function (comic, comicList) {
-    var title = '<button class="comicButton" id=' + comic.title + '>' + comic.title + '</button>';
+    var title = '<button class="comicButton" id=' + comic.title.toLowerCase() + '>' + comic.title + '</button>';
     $('.panel-list').append(title);
-    showDetails(comic.title, comicList);
+    showDetails(comic.title.toLowerCase(), comicList);
   };
 
   var showDetails = function (idButton, comicList) {
     var button = document.getElementById(idButton)
     $(button).click(function () {
-      $('#detailComic').show(1500);
+       $('#commentsComic').html("");
       for (var index = 0; index < comicList.length; index++) {
-        if (idButton === comicList[index].title) {
+        if (idButton === comicList[index].title.toLowerCase()) {
           $('#titleComic').html(comicList[index].title)
           $('#descriptionComic').html(comicList[index].description)
-          $('#commentsComic').html(comicList[index].comments)
+          for (var i = 0; i < comicList[index].comments.length; i++) {
+            $('#commentsComic').append('<p>' + comicList[index].comments[i] + '</p>')
+          }
         }
       }
+      $('#detailComic').show(1500);
     });
   }
 
