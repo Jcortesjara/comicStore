66ab6e868b1170398d7e806a9c51ab5e984e6d5a
Jose  Cortes Jaramillo <josecortesjaramillo@gmail.com>
2016-08-15 22:32:00 -0500

    add minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,33 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+        files: {
+          'dist/js/libs.min.js': ['node_modules/jquery/dist/jquery.js'],
+          'dist/js/comic.min.js': ['js/*.js']
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css'],
+          dest: 'dist/css',
+          ext: '.min.css'
+        }]
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
